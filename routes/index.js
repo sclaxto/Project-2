15512af8778dca13618687d4cfdd079fc79a4bdd
snclaxto <snclaxto@gmail.com>
2022-04-15 14:07:23 -0400

    fixed logout

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.get('/oauth2callback', passport.authenticate(
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout();
-  res.redirect('/index');
+  res.redirect('/');
 });
 
 module.exports = router;
